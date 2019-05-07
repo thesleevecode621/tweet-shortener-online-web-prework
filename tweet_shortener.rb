431632153727c30c14575d8966fadf0fc35d7bf6
boruch d <dubywider@gmail.com>
2019-05-07 16:47:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -36,7 +36,7 @@ end
     tweet
   end
 end
-ef shortened_tweet_truncator(tweet)
+def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
     word_substituter(tweet)[0..136] + ("...")
   else
